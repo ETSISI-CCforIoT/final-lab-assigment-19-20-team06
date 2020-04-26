9ff44e10cfecb4463c32a91348d93abafc34e9cd
Carlos Medina <c.medinam@alumnos.upm.es>
2020-04-27 00:25:05 +0200

    Added Dharma Lancelot <dl.martinez@alumnos.upm.es> mqtt implementation to separate devices

--- pipes-devices/devices/python/main/src/flowmeter_client.py
+++ pipes-devices/devices/python/main/src/flowmeter_client.py
@@ -1,11 +1,21 @@
 import asyncio
+import time
 import json
 import logging
+import paho.mqtt.client as mqtt
+
+MQTT_BROKER_IP = '127.0.0.1'
+MQTT_BROKER_PORT = 1883
+MQTT_BROKER_TOPIC = 'floors/floor1/data'
 
 FE_SERVER_HOST = '127.0.0.1'
 FE_SERVER_PORT = 8888
 
-CLIENT_ID = 'WATER_IN_1'
+# TODO CLIENT_ID and PIPE_ID can be the same if the names in the simulator match with names in the cloud
+CLIENT_ID = 'PIPE_1_S1'
+PIPE_ID = 'pipe1-sensor1'
+REFRESH_PERIOD = 5
+
 
 async def tcp_connect():
     logger = logging.getLogger(CLIENT_ID)
@@ -19,8 +29,9 @@ async def tcp_send_package(rdr, wtr, package):
     logger.info('Send: %s', package)
     wtr.write(package.encode())
 
-    data = await rdr.read(100)
+    data = await rdr.read(1024)
     logger.info('Received: %s', data.decode())
+    return data
 
 
 async def tcp_close(wtr):
@@ -29,8 +40,7 @@ async def tcp_close(wtr):
     wtr.close()
 
 
-async def main():
-    ## Init mqtt client
+async def main(mqtt_client):
     
     while True:
         try:
@@ -39,21 +49,53 @@ async def main():
                 'command': 'getComp',
                 'arguments': CLIENT_ID
             }
-            await tcp_send_package(reader, writer, json.dumps(packet))
+            raw_response = await tcp_send_package(reader, writer, json.dumps(packet))
             await tcp_close(writer)
 
-            ##mqtt.publish
+            response = json.loads(raw_response)
+            if response['status'] == 'OK':
+                mqtt_client.publish(MQTT_BROKER_TOPIC, payload=compose_packet(response['flow'], response['units']))
 
-            await asyncio.sleep(5)
+            await asyncio.sleep(REFRESH_PERIOD)
         except ConnectionRefusedError:
             logger = logging.getLogger(CLIENT_ID)
             logger.info('Connection Refused to %s:%d', FE_SERVER_HOST, FE_SERVER_PORT)
 
-logging.basicConfig(level=logging.INFO)
 
-loop = asyncio.get_event_loop()
+def on_connect(client, userdata, flags, rc):
+    logger = logging.getLogger(CLIENT_ID+"_MQTT")
+    logger.info('Connected to %s:%d with code %s', MQTT_BROKER_IP, MQTT_BROKER_PORT, rc)
+
+
+def on_disconnect(client, userdata, rc, properties):
+    logger = logging.getLogger(CLIENT_ID+"_MQTT")
+    logger.info('Disconnected from %s:%d with code %s', MQTT_BROKER_IP, MQTT_BROKER_PORT, rc)
+
+
+def compose_packet(flow, unit):
+    packet = {
+        "id": PIPE_ID,
+        "flow": round(flow, 4),
+        "unit": unit,
+        "timestamp": time.time()
+    }
+    return json.dumps(packet)
+
+
+logging.basicConfig(level=logging.INFO)
+MQTT_CLIENT = mqtt.Client(client_id=CLIENT_ID)
+
+MQTT_CLIENT.on_connect = on_connect
+MQTT_CLIENT.on_disconnect = on_disconnect
+
+MQTT_CLIENT.connect(MQTT_BROKER_IP, port=MQTT_BROKER_PORT)
+MQTT_CLIENT.loop_start()
+
+LOOP = asyncio.get_event_loop()
 
 try:
-    loop.run_until_complete(main())
+    LOOP.run_until_complete(main(MQTT_CLIENT))
 except KeyboardInterrupt:
-    loop.stop()
+    LOOP.stop()
+    MQTT_CLIENT.loop_stop()
+    MQTT_CLIENT.disconnect()
--- pipes-devices/devices/python/main/src/valve_client.py
+++ pipes-devices/devices/python/main/src/valve_client.py
@@ -2,13 +2,20 @@ import asyncio
 import json
 import logging
 import random
+import paho.mqtt.client as mqtt
 
 random.seed()
+
+MQTT_BROKER_IP = '127.0.0.1'
+MQTT_BROKER_PORT = 1883
+MQTT_BROKER_TOPIC = 'floors/floor1/alarms'
 
 FE_SERVER_HOST = '127.0.0.1'
 FE_SERVER_PORT = 8888
 
 CLIENT_ID = 'WATER_IN_1'
+CURRENT_VALUE = 100.0
+
 
 async def tcp_connect():
     logger = logging.getLogger(CLIENT_ID)
@@ -32,34 +39,80 @@ async def tcp_close(wtr):
     wtr.close()
 
 
-async def main():
-    ## Init 
-    
+async def main():  # global CURRENT_VALUE reader function
+    global CURRENT_VALUE
+    logger = logging.getLogger(CLIENT_ID+"_MQTTVALVE")
+    logger.info('Press Ctrl-C to exit...')
+    previous_val = CURRENT_VALUE
     while True:
-        try:
-            reader, writer = await tcp_connect()
-            packet = {
-                'command': 'setComp',
-                'arguments': {
-                               'c_name': CLIENT_ID,
-                               'c_values': [float(random.randint(0, 5))*10.0+50.0, 0.0, 1.0]
-                              }
-            }
-            await tcp_send_package(reader, writer, json.dumps(packet))
-            await tcp_close(writer)
+        if previous_val != CURRENT_VALUE:
+            try:
+                reader, writer = await tcp_connect()
+                packet = {
+                    'command': 'setComp',
+                    'arguments': {
+                                   'c_name': CLIENT_ID,
+                                   'c_values': [CURRENT_VALUE, 0.0, 1.0]
+                                  }
+                }
+                await tcp_send_package(reader, writer, json.dumps(packet))
+                await tcp_close(writer)
 
-            ##mqtt.publish
+                previous_val = CURRENT_VALUE
 
-            await asyncio.sleep(1)
-        except ConnectionRefusedError:
-            logger = logging.getLogger(CLIENT_ID)
-            logger.info('Connection Refused to %s:%d', FE_SERVER_HOST, FE_SERVER_PORT)
+                await asyncio.sleep(1)
+            except ConnectionRefusedError:
+                logger = logging.getLogger(CLIENT_ID)
+                logger.info('Connection Refused to %s:%d', FE_SERVER_HOST, FE_SERVER_PORT)
+
+
+def on_connect(client, userdata, flags, rc):
+    logger = logging.getLogger(CLIENT_ID+"_MQTTVALVE")
+    logger.info('Connected to %s:%d with code %s', MQTT_BROKER_IP, MQTT_BROKER_PORT, rc)
+    client.subscribe(MQTT_BROKER_TOPIC)
+
+
+def on_disconnect(client, userdata, rc, properties):
+    logger = logging.getLogger(CLIENT_ID+"_MQTTVALVE")
+    logger.info('Disconnected from %s:%d with code %s', MQTT_BROKER_IP, MQTT_BROKER_PORT, rc)
+
+
+def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+    logger = logging.getLogger(CLIENT_ID+"_MQTTVALVE")
+    logger.info('Subscribed with mid %s', mid)
+
+# TODO Put the data from payload as new CURRENT_VALUE
+
+
+def on_message(client, userdata, message):  # global CURRENT_VALUE writer function
+    global CURRENT_VALUE
+    logger = logging.getLogger(CLIENT_ID + "_MQTTVALVE")
+    logger.info('-'*30)
+    logger.info('\t topic: %s', message.topic)
+    logger.info('\t payload: %s', message.payload)
+    logger.info('\t qos: %d ', message.qos)
+    temp = float(random.randint(0, 5)) * 10.0 + 50.0
+    logger.info('PUMP: SET PUMP %s POWER TO %s', CLIENT_ID, temp)
+    CURRENT_VALUE = temp
+    logger.info('-'*30)
+
 
 logging.basicConfig(level=logging.INFO)
+MQTT_CLIENT = mqtt.Client(client_id=CLIENT_ID)
 
-loop = asyncio.get_event_loop()
+MQTT_CLIENT.on_connect = on_connect
+MQTT_CLIENT.on_disconnect = on_disconnect
+MQTT_CLIENT.on_message = on_message
+MQTT_CLIENT.on_subscribe = on_subscribe
+
+MQTT_CLIENT.connect(MQTT_BROKER_IP, port=MQTT_BROKER_PORT)
+MQTT_CLIENT.loop_start()
+
+LOOP = asyncio.get_event_loop()
 
 try:
-    loop.run_until_complete(main())
+    LOOP.run_until_complete(main())
 except KeyboardInterrupt:
-    loop.stop()
+    LOOP.stop()
+    MQTT_CLIENT.loop_stop()
+    MQTT_CLIENT.disconnect()
